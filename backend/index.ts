3653c54a7fda9d0e189a343b43f55befec4eba24
Jakob Molin <jakob.molin@folksam.se>
2023-03-25 20:47:55 +0100

    added booking and health link

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import healthRouter from './src/routes/health.routes';
+import bookingRouter from './src/routes/booking.routes';
 
 dotenv.config();
 
@@ -9,6 +11,11 @@ const port = process.env.PORT;
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
 });
+
+app.use('/health', healthRouter);
+app.use('/booking', bookingRouter);
+app.use(healthRouter);
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ backend/src/routes/index.ts
@@ -0,0 +1,1 @@
+const API_BASE_PATH = 'http://localhost:4000';
