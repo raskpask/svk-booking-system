eb4fabe1154a3724ce0349721f475cfd1a64d02b
Jakob Molin <jakob.molin@folksam.se>
2023-04-08 21:23:30 +0200

    remoed backend

--- backend/src/model/user.ts
+++
@@ -1,9 +0,0 @@
-import { AccessLevel } from "./enums/accessLevel";
-
-export interface User {
-    accessLevel: AccessLevel;
-    firstName: string;
-    lastName: string;
-    email: string;
-    phone: string;
-  }
