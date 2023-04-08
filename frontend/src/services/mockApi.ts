eed1b69fb350e5a2dfa216afc79eb7b9d9e1ec6f
Tim Skogsfors <tim.skogsfors@customerfirst.se>
2023-04-08 22:41:32 +0200

    commit

--- frontend/src/services/mockApi.ts
+++ frontend/src/services/mockApi.ts
@@ -5,21 +5,23 @@ export async function fetchAvailableTimeSlots(date: Date): Promise<any> {
   const dummyData = [
     {
       courtName: "Court 1",
+      UUID: "303d94e8-c628-43c5-a6a1-8407f0292b7e",
       timeSlots: [
-        { time: "09:00", available: true, courtId: 1 },
-        { time: "10:00", available: false, courtId: 1 },
-        { time: "11:00", available: true, courtId: 1 },
-        { time: "12:00", available: true, courtId: 1 },
+        { time: "09:00", available: true, courtId: 1, id: 0 },
+        { time: "10:00", available: false, courtId: 1, id: 1 },
+        { time: "11:00", available: true, courtId: 1, id: 2 },
+        { time: "12:00", available: true, courtId: 1, id: 3 },
         // More time slots if needed...
       ],
     },
     {
       courtName: "Court 2",
+      UUID: "091ccd90-3c1e-44a1-bb98-99132dbb33a9",
       timeSlots: [
-        { time: "09:00", available: true, courtId: 2 },
-        { time: "10:00", available: true, courtId: 2 },
-        { time: "11:00", available: false, courtId: 2 },
-        { time: "12:00", available: true, courtId: 2 },
+        { time: "09:00", available: true, courtId: 2, id: 0 },
+        { time: "10:00", available: true, courtId: 2, id: 1 },
+        { time: "11:00", available: false, courtId: 2, id: 2 },
+        { time: "12:00", available: true, courtId: 2, id: 3 },
         // More time slots if needed...
       ],
     },
